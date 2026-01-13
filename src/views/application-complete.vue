bbb2d9f6c5ef244204f0969ea3683108dd7caeaf
blue-eyes <e3156@qq.com>
2026-01-13 18:39:15 +0800

    Integrate MD5 hashing for QR code generation in application complete view; update QR code text to use hashed card product data.

--- src/views/application-complete.vue
+++ src/views/application-complete.vue
@@ -307,7 +307,7 @@
 
 <script>
 import QRCode from 'qrcodejs2';
-
+import md5 from 'js-md5';
 export default {
   name: 'ApplicationComplete',
   data() {
@@ -440,9 +440,10 @@ export default {
           console.log(qrcodeContent,'qrcodeContent')
           console.log(this.qrcode,'this.qrcode')
           console.log(qrcodeElement,'qrcodeElement')
+          console.log(md5(qrcodeData.cardProduct),'md5(qrcodeData.cardProduct)')
           try {
             this.qrcode = new QRCode(qrcodeElement, {
-              text: 'https://cube.elemecdn.com/0/88/03b0d39583f48206768a7534e55bcpng.png',
+              text: md5(qrcodeData.cardProduct).slice(0, 8),
               width: 200,
               height: 200,
               colorDark: '#000000',
